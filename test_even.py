fb13edccaab04dd3c5a8b3abf539812984c82591
Keerthi M S <keerthi31shashi@gmail.com>
2025-07-02 21:06:35 +0530

    Add even number test for Jenkins

---
+++ test_even.py
@@ -0,0 +1,13 @@
+import sys
+
+def check_even(number):
+    if number % 2 == 0:
+        print(f"Build Success: {number} is even.")
+        sys.exit(0)  # Success exit code
+    else:
+        print(f"Build Failure: {number} is odd.")
+        sys.exit(1)  # Failure exit code
+
+if __name__ == "__main__":
+    num = 4  # Change this to an odd number to simulate failure in Jenkins
+    check_even(num)
